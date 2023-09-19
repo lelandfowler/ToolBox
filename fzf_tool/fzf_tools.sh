f569da9e68a45c33e872826ba13e252e38b29e23
Leland Fowler <lelandplusplus@gmail.com>
2023-09-19 11:06:39 -0700

    chore: clean up fzf_tool and simplify script.

--- fzf_tool/fzf_tools.sh
+++ fzf_tool/fzf_tools.sh
@@ -1,31 +1,22 @@
 #!/bin/bash
 
-# Define the fzf interactive function
 fzf_interactive() {
     [ -n "$READLINE_LINE" ] && local QUERY="-q $READLINE_LINE" || local QUERY=""
     local COMMAND=$(history | awk '{$1=""; print substr($0, 2)}' | awk '!x[$0]++' | fzf $QUERY --tac --height 10%)
     READLINE_LINE=$COMMAND
     READLINE_POINT=${#COMMAND}
-
-    # Update the history ignore pattern to ignore commands starting with 'fh'
-    export HISTIGNORE="$HISTIGNORE:fh*"
 }
 
 # Bind the Ctrl+f key and Ctrl+Alt+f to the fzf interactive function
 bind -x '"\C-f": fzf_interactive'
 bind -x '"\e\C-f": fzf_interactive'
 
-
-
-# Define the fh function
 fh() {
     local LINES=$1
     local QUERY=""
 
-    # Default
-    if [ -z "$1" ]; then
-        LINES=5
-    elif ! [[ $LINES =~ ^[0-9]+$ ]]; then
+    # Default value for LINES if not provided or not a positive integer
+    if [ -z "$1" ] || ! [[ $LINES =~ ^[0-9]+$ ]]; then
         LINES=5
     fi
 
@@ -38,7 +29,4 @@ fh() {
     echo "______________________________________________"
     history | grep -E -B$LINES -A$LINES "^[ ]+$CMD_NUM "
     echo "______________________________________________"
-
-    # Update the history ignore pattern to ignore commands starting with 'fh'
-    export HISTIGNORE="$HISTIGNORE:fh*"
 }
