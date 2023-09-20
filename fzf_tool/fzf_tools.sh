105d230d2ed663b586e36d9c0513ac4200e281f0
Leland Fowler <lelandplusplus@gmail.com>
2023-09-20 13:39:07 -0700

    feat: Fix fzf logic to improve functionality.

--- fzf_tool/fzf_tools.sh
+++ fzf_tool/fzf_tools.sh
@@ -1,40 +1,39 @@
-#!/bin/bash
-
 fzf_interactive() {
-    if [ -n "$READLINE_LINE" ]; then
-        local QUERY="-q $READLINE_LINE"
-    else
-        local QUERY=""
-    fi
-    local COMMAND
-    COMMAND=$(history | awk '{$1=""; print substr($0, 2)}' | awk '!x[$0]++' | fzf "$QUERY" --tac --height 10%)
-    READLINE_LINE=$COMMAND
-    READLINE_POINT=${#COMMAND}
+  [ -n "$READLINE_LINE" ] && local QUERY="-q $READLINE_LINE" || local QUERY=""
+  local COMMAND=$(history | awk '{$1=""; print substr($0, 2)}' | awk '!x[$0]++' | fzf $QUERY --tac --height 10%)
+  READLINE_LINE=$COMMAND
+  READLINE_POINT=${#COMMAND}
 }
 
-
-# Bind the Ctrl+f key and Ctrl+Alt+f to the fzf interactive function
 bind -x '"\C-f": fzf_interactive'
-bind -x '"\e\C-f": fzf_interactive'
+
+export HISTIGNORE="$HISTIGNORE:fh*"
 
 fh() {
-    local LINES=$1
-    local QUERY=""
+  local LINES=$1
+  local QUERY=""
+  
+  # If no arguments were provided, LINES is set to 0
+  if [ -z "$1" ]; then
+    LINES=5
+  fi
 
-    # Default value for LINES if not provided or not a positive integer
-    if [ -z "$1" ] || ! [[ $LINES =~ ^[0-9]+$ ]]; then
-        LINES=5
-    fi
+  # User selects a command, and we get the command number and the command
+  local SELECTED=$(history | fzf $QUERY --tac --height 40%)
+  local CMD_NUM=$(echo "$SELECTED" | awk '{print $1}')
+  local CMD=$(echo "$SELECTED" | awk '{$1=""; print substr($0, 2)}')
 
+  # If LINES is not a positive integer, set it to 0
+  if ! [[ $LINES =~ ^[0-9]+$ ]]; then
+    LINES=5
+  fi
 
-    # User selects a command, and we get the command number and the command
-    local SELECTED
-    local CMD_NUM
-    SELECTED=$(history | fzf "$QUERY" --tac --height 40%)
-    CMD_NUM=$(echo "$SELECTED" | awk '{print $1}')
-
-    # Display the lines around the selected command
-    echo "______________________________________________"
-    history | grep -E -B$LINES -A$LINES "^[ ]+$CMD_NUM "
-    echo "______________________________________________"
+  # Display the lines around the selected command
+  echo "______________________________________________"
+  history | grep -E -B$LINES -A$LINES "^[ ]+$CMD_NUM "
+  echo "______________________________________________"
 }
+
+export HISTIGNORE="$HISTIGNORE:fh*"
+[ -f ~/.fzf.bash ] && source ~/.fzf.bash
+
